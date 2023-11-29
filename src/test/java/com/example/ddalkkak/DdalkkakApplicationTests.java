2a2d9cde9ef553dd2127bec9fbfd3ba3be67a3b3
Chan <ieejo716@naver.com>
2023-11-29 16:58:52 +0900

    init setting

---
+++ src/test/java/com/example/ddalkkak/DdalkkakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddalkkak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdalkkakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
